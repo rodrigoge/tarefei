fe8cb3a5db3c408b3c8eb3e22c290b4910a54efd
Rodrigo Gouveia <rgestevao@gmail.com>
2025-03-20 13:44:24 -0300

    fix: reinstall prisma and other dependencies

--- tarefei/apps/api/src/app.ts
+++ tarefei/apps/api/src/app.ts
@@ -1,7 +1,5 @@
 import cors from 'cors';
 import express from 'express';
-import swaggerUi from 'swagger-ui-express';
-import { swaggerDocs } from './configs/swaggerConfig';
 import { router } from './routes';
 
 const app = express()
@@ -9,7 +7,6 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(router)
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 export { app };
 
